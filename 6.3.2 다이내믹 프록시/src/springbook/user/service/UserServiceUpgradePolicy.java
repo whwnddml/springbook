feb3965290817f22b16587632503fb00e5635666
whwnddml <junny4u@gmail.com>
2015-01-07 12:46:03 +0900

    Revert "start"
    This reverts commit 82093f3b0a28958689cd3fb93c17868ddd787c0e.

--- 6.3.2 다이내믹 프록시/src/springbook/user/service/UserServiceUpgradePolicy.java
+++
@@ -1,8 +0,0 @@
-package springbook.user.service;
-
-import springbook.user.domain.User;
-
-public interface UserServiceUpgradePolicy {
-	boolean canUpgradeLevel(User user);
-	void upgradeLevel(User user);
-}
